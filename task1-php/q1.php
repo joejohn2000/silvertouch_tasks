de328a7c5b9a167f8ce5735a50e0e7eaedc93de6
joe john parakka <65030989+joejohn2000@users.noreply.github.com>
2023-03-17 16:52:30 +0530

    Update q1.php

--- task1-php/q1.php
+++ task1-php/q1.php
@@ -8,7 +8,7 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.0/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 <link rel="stylesheet" href="style.css">
-<title>p1</title>
+<title>q1</title>
 </head>
 <body>
 
